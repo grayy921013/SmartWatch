902f4b31a9316730313e1a56c5ca40bb3df8d5d0
vincent zhou <grayy921013@gmail.com>
2016-03-27 00:10:38 +0800

    setting page refined & notification added

---
+++ Test/MainInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainInfoViewController.h
+//  Test
+//
+//  Created by vincent on 3/26/16.
+//  Copyright © 2016 vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainInfoViewController : UIViewController
+
+@end
